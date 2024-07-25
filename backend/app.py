142c9666e62652bfa58a8d22063628e36edd24e2
adtechGodoStrategies <adtech@godostrategies.com>
2024-07-25 10:19:02 +0200

    frist commit

---
+++ backend/app.py
@@ -0,0 +1,342 @@
+from flask import Flask, request, jsonify, Response
+from flask_cors import CORS
+import _locale
+import re
+from datetime import datetime
+import time
+from googleads import ad_manager
+
+_locale._getdefaultlocale = (lambda *args: ['en_US', 'UTF-8'])
+
+app = Flask(__name__)
+CORS(app, supports_credentials=True)
+
+# Load the Ad Manager client details from stored authorization.
+ad_manager_client = ad_manager.AdManagerClient.LoadFromStorage()
+
+# Connect to the Ad Manager network
+network_service = ad_manager_client.GetService(
+    'NetworkService', version='v202405')
+
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    data = request.get_json()
+    orden = data.get('orden')
+    price = float(data.get('price', 12.0))
+    limit = float(data.get('limit', 20.0))
+    line_name_template = data.get('line_name_template')
+    lineItemType = data.get('lineItemType')
+    priority = data.get('priority')
+    inventory = data.get('inventory')
+    expectedCreative = data.get('expectedCreative')
+    deliverySettings = data.get('deliverySettings')
+    customTime = data.get('customTime')
+    endSettings = data.get('endSettings')
+    endDate = data.get('endDate')
+    endTime = data.get('endTime')
+
+    # Mapeo de inventario a adUnitId
+    inventory_map = {
+        'LV': '62332164',
+        'MD': '57362484',
+        'RAC1': '70115964'
+    }
+
+    if 'include_' in inventory:
+        inventory_key = inventory.split('include_')[1]
+        adUnitId = inventory_map.get(inventory_key)
+    elif inventory == 'exclude':
+        adUnitId = 'exclude_adUnitId'
+    else:
+        return jsonify({'error': 'Invalid inventory'}), 400
+
+    if not adUnitId:
+        return jsonify({'error': 'Invalid inventory'}), 400
+
+    print(lineItemType)
+    print(priority)
+
+    if expectedCreative == '728x90':
+        creative_placeholder_size = {'width': '728', 'height': '90'}
+    elif expectedCreative == '1x1':
+        creative_placeholder_size = {'width': '1', 'height': '1'}
+    else:
+        return jsonify({'error': 'Invalid creative size'}), 400
+
+    if deliverySettings == 'IMMEDIATELY':
+        startDateTimeType = 'IMMEDIATELY'
+        startDateTime = None
+    else:
+        try:
+            startDateTime = datetime.strptime(
+                f"{deliverySettings} {customTime}", '%d/%m/%Y %H:%M')
+            startDateTimeType = None
+        except ValueError:
+            return jsonify({'error': 'Invalid date or time format'}), 400
+
+    if endSettings == 'UNLIMITED':
+        unlimitedEndDateTime = True
+        endDateTime = None
+    else:
+        unlimitedEndDateTime = False
+        try:
+            endDateTime = datetime.strptime(
+                f"{endDate} {endTime}", '%d/%m/%Y %H:%M')
+        except ValueError:
+            return jsonify({'error': 'Invalid end date or time format'}), 400
+
+    filename = f"line_items_create_lines_lv_anot_{str(price)}_to_{
+        str(limit)}.txt"
+    with open(filename, "a") as f:
+        updated_count = 0
+
+        def select_Network():
+            test_redes = network_service.getAllNetworks()
+            for i, network in enumerate(test_redes):
+                f.write(f"{i}. Network '{network['networkCode']}' '{
+                        network['displayName']}'\n")
+                f.write("\n")
+            num_red = 0  # Assuming the first network for simplicity
+            current_network = test_redes[num_red]
+            ad_manager_client.network_code = current_network['networkCode']
+            f.write(f"\nCurrent network: {
+                    current_network['networkCode']} - {current_network['displayName']}\n")
+            print(f"Current network: {
+                  current_network['networkCode']} - {current_network['displayName']}")
+
+        def select_and_print_orders(order_id):
+            order_service = ad_manager_client.GetService(
+                'OrderService', version='v202405')
+            statement = (ad_manager.StatementBuilder().Where(
+                'id = :id').WithBindVariable('id', int(order_id)).Limit(1))
+            response = order_service.getOrdersByStatement(
+                statement.ToStatement())
+
+            if 'results' in response and len(response['results']):
+                order = response['results'][0]
+                f.write(f"\nWorking with Order {order_id} - {order['name']}\n")
+                print(f"Working with Order {order_id} - {order['name']}")
+            else:
+                f.write(f"No order found with ID {order_id}")
+                print(f"No order found with ID {order_id}")
+                return None
+            return order_id
+
+        def select_and_print_lines(order_id):
+            line_item_service = ad_manager_client.GetService(
+                'LineItemService', version='v202405')
+            statement = (ad_manager.StatementBuilder().Where(
+                'orderId = :orderId').WithBindVariable('orderId', int(order_id)))
+            response = line_item_service.getLineItemsByStatement(
+                statement.ToStatement())
+            line_items = getattr(response, 'results', [])
+            f.write(f"Found {len(line_items)
+                             } line items for Order {order_id}\n")
+            print(f"Found {len(line_items)} line items for Order {order_id}")
+            for line in line_items:
+                f.write(f"Name: {line['name']}\nID: {line['id']}\n")
+                print(f"Name: {line['name']}\nID: {line['id']}")
+            return line_items
+
+        def create_line_item(order_id, line_name, price, id_price):
+            line_item_service = ad_manager_client.GetService(
+                'LineItemService', version='v202405')
+            line_item = {
+                'name': line_name,
+                'orderId': order_id,
+                'startDateTimeType': startDateTimeType,
+                'unlimitedEndDateTime': unlimitedEndDateTime,
+                'creativeRotationType': 'EVEN',
+                'roadblockingType': 'AS_MANY_AS_POSSIBLE',
+                'childContentEligibility': 'ALLOWED',
+                'allowOverbook': 'true',
+                'skipInventoryCheck': 'true',
+                'creativePlaceholders': [{'size': creative_placeholder_size, 'creativeSizeType': 'PIXEL'}],
+                'lineItemType': lineItemType,
+                'priority': priority,
+                'deliveryRateType': 'EVENLY',
+                'targeting': {
+                    'geoTargeting': {
+                        'targetedLocations': [{'id': '2724', 'displayName': 'SPAIN'}, {'id': '2484', 'displayName': 'MEXICO'}]
+                    },
+                    'inventoryTargeting': {
+                        'targetedAdUnits': [{'adUnitId': adUnitId}],
+                        'excludedAdUnits': [{'adUnitId': '66970884'}, {'adUnitId': '21799279351'}, {'adUnitId': '21960565090'}, {'adUnitId': '22069771456'}]
+                    },
+                    'customTargeting': {
+                        'xsi_type': 'CustomCriteriaSet',
+                        'logicalOperator': 'OR',
+                        'children': [
+                            {
+                                'xsi_type': 'CustomCriteriaSet',
+                                'logicalOperator': 'AND',
+                                'children': [
+                                    {'xsi_type': 'CustomCriteria', 'keyId': 147444, 'operator': 'IS', 'valueIds': [
+                                        208473659844, 189633282084, 174650883684]},
+                                    {'xsi_type': 'CustomCriteria', 'keyId': 147684,
+                                        'operator': 'IS', 'valueIds': [84198161484]},
+                                    {'xsi_type': 'CustomCriteria', 'keyId': 154044,
+                                        'operator': 'IS', 'valueIds': [104139487764]},
+                                    {'xsi_type': 'CustomCriteria', 'keyId': 12084959,
+                                        'operator': 'IS', 'valueIds': [448206030155]},
+                                    {'xsi_type': 'CustomCriteria', 'keyId': 11921921, 'operator': 'IS', 'valueIds': [
+                                        448131355960, 448148134169]},
+                                    {'xsi_type': 'CustomCriteria', 'keyId': 147444,
+                                        'operator': 'IS_NOT', 'valueIds': [448561956455]},
+                                    {'xsi_type': 'CustomCriteria', 'keyId': 217884,
+                                        'operator': 'IS', 'valueIds': [id_price]}
+                                ]
+                            }
+                        ]
+                    }
+                },
+                'costType': 'CPM',
+                'costPerUnit': {'currencyCode': 'EUR', 'microAmount': "{:.0f}".format(price * 1000000)},
+                'primaryGoal': {'goalType': 'DAILY', 'units': 100}
+            }
+
+            if startDateTime:
+                line_item['startDateTime'] = {
+                    'date': {
+                        'year': startDateTime.year,
+                        'month': startDateTime.month,
+                        'day': startDateTime.day
+                    },
+                    'hour': startDateTime.hour,
+                    'minute': startDateTime.minute,
+                    'second': 0,
+                    'timeZoneId': 'Europe/Madrid'
+                }
+
+            if endDateTime:
+                line_item['endDateTime'] = {
+                    'date': {
+                        'year': endDateTime.year,
+                        'month': endDateTime.month,
+                        'day': endDateTime.day
+                    },
+                    'hour': endDateTime.hour,
+                    'minute': endDateTime.minute,
+                    'second': 0,
+                    'timeZoneId': 'Europe/Madrid'
+                }
+
+            created_line_item = line_item_service.createLineItems([line_item])[
+                0]
+            f.write(f"New Line Item created with ID {
+                    created_line_item['id']} and name {created_line_item['name']}\n")
+            print(f"New Line Item created with ID {
+                  created_line_item['id']} and name {created_line_item['name']}")
+            return created_line_item['id'], created_line_item['name']
+
+        def create_creative(order_id, width, height):
+            creative_service = ad_manager_client.GetService(
+                'CreativeService', version='v202405')
+            creative = {
+                'xsi_type': 'TemplateCreative',
+                'name': f'Creative Rich Audience Header bidding',
+                'advertiserId': get_advertiser_id(order_id),
+                'size': {'width': width, 'height': height},
+                'creativeTemplateId': 12262965,
+            }
+            created_creative = creative_service.createCreatives([creative])[0]
+            f.write(f"New Creative created with ID {
+                    created_creative['id']} and name {created_creative['name']}\n")
+            print(f"New Creative created with ID {
+                  created_creative['id']} and name {created_creative['name']}")
+            return created_creative['id']
+
+        def create_creative_item_120_600(order_id):
+            creative_service = ad_manager_client.GetService(
+                'CreativeService', version='v202405')
+            with open('img/1x1.jpg', 'rb') as image:
+                image_data = image.read()
+            creative_asset = {'xsi_type': 'CreativeAsset',
+                              'fileName': 'image.jpg', 'assetByteArray': image_data}
+            creative = {
+                'xsi_type': 'ImageCreative',
+                'name': f'120x600',
+                'advertiserId': get_advertiser_id(order_id),
+                'size': {'width': '120', 'height': '600'},
+                'primaryImageAsset': creative_asset,
+                'destinationUrl': 'https://www.lavanguardia.com/',
+                'overrideSize': True,
+            }
+            created_creative = creative_service.createCreatives([creative])[0]
+            f.write(f"New Creative created with ID {
+                    created_creative['id']} and name {created_creative['name']}\n")
+            print(f"New Creative created with ID {
+                  created_creative['id']} and name {created_creative['name']}")
+            return created_creative['id']
+
+        def association_line_with_creative(created_line_item, created_creative):
+            line_item_creative_assosiation_service = ad_manager_client.GetService(
+                'LineItemCreativeAssociationService', version='v202405')
+            creative_association = {
+                'creativeId': created_creative, 'lineItemId': created_line_item}
+            line_item_creative_assosiation_service.createLineItemCreativeAssociations([
+                                                                                      creative_association])
+            f.write(f"Creative assigned to Line Item with ID {
+                    created_creative}\n")
+            print(f"Creative assigned to Line Item with ID {created_creative}")
+
+        def get_advertiser_id(order_id):
+            statement = (ad_manager.StatementBuilder().Where(
+                'orderId = :orderId').WithBindVariable('orderId', int(order_id)))
+            order_service = ad_manager_client.GetService(
+                'OrderService', version='v202405')
+            responseOrder = order_service.getOrdersByStatement(
+                statement.ToStatement())
+            order = responseOrder['results'][0]
+            advertiser_id = order['advertiserId']
+            return advertiser_id
+
+        def get_all_hb_pb(price):
+            target_name_value = price
+            custom_targeting_service = ad_manager_client.GetService(
+                'CustomTargetingService', version='v202405')
+            value_statement = (ad_manager.StatementBuilder().Where(
+                'customTargetingKeyId = :keyId').WithBindVariable('keyId', int(217884)))
+            value_statement.Offset(0).Limit(500)
+            values_response = custom_targeting_service.getCustomTargetingValuesByStatement(
+                value_statement.ToStatement())
+            while True:
+                if 'results' in values_response:
+                    for value in values_response['results']:
+                        targeting_value_id = value['id']
+                        targeting_value_name = value['name']
+                        if str(targeting_value_name).strip() == str(target_name_value).strip():
+                            return targeting_value_id
+                    value_statement.Offset(value_statement.offset + 100)
+                    values_response = custom_targeting_service.getCustomTargetingValuesByStatement(
+                        value_statement.ToStatement())
+                else:
+                    break
+
+        select_Network()
+        order_id = select_and_print_orders(orden)
+        if not order_id:
+            return jsonify({'error': 'Order not found'}), 404
+
+        line_items = select_and_print_lines(order_id)
+        while price <= limit:
+            id_price = get_all_hb_pb("{:.2f}".format(price))
+            line_name = f"{line_name_template} {price}"
+
+            line_item_id, line_item_name = create_line_item(
+                order_id, line_name, price, id_price)
+            for n in range(1, 2):
+                creative_id = create_creative(
+                    order_id, creative_placeholder_size['width'], creative_placeholder_size['height'])
+                association_line_with_creative(line_item_id, creative_id)
+            price = round(price + 0.50, 2)
+            updated_count += 1
+
+        f.write(f"Total line items updated: {updated_count}\n")
+        print(f"Total line items updated: {updated_count}")
+        return jsonify({'resultado': f"Líneas creadas correctamente. Total line items updated: {updated_count}"})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- src/App.js
+++ src/App.js
@@ -1,23 +1,233 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [orden, setOrden] = useState('');
+  const [price, setPrice] = useState(12.0);
+  const [limit, setLimit] = useState(20.0);
+  const [lineNameTemplate, setLineNameTemplate] = useState('');
+  const [lineItemType, setLineItemType] = useState('');
+  const [priority, setPriority] = useState('');
+  const [inventoryOption, setInventoryOption] = useState('');
+  const [inventoryInclude, setInventoryInclude] = useState('');
+  const [expectedCreative, setExpectedCreative] = useState('');
+  const [deliverySettings, setDeliverySettings] = useState('IMMEDIATELY');
+  const [customDate, setCustomDate] = useState('');
+  const [customTime, setCustomTime] = useState('00:00');
+  const [endSettings, setEndSettings] = useState('UNLIMITED');
+  const [endDate, setEndDate] = useState('');
+  const [endTime, setEndTime] = useState('00:00');
+  const [resultado, setResultado] = useState('');
+  const [lineItems, setLineItems] = useState([]);
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setIsGenerating(true);
+    setLineItems([]);
+    setResultado('');
+
+    let inventory = inventoryOption;
+    if (inventoryOption === 'include') {
+      inventory = `include_${inventoryInclude}`;
+    }
+
+    let deliverySetting = deliverySettings;
+    if (deliverySettings === 'CUSTOM') {
+      deliverySetting = customDate;
+    }
+
+    const response = await fetch('http://127.0.0.1:5000/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ 
+        orden, 
+        price, 
+        limit, 
+        line_name_template: lineNameTemplate,
+        lineItemType: lineItemType.toUpperCase(),
+        priority,
+        inventory,
+        expectedCreative,
+        deliverySettings: deliverySetting,
+        customTime: customTime,
+        endSettings: endSettings,
+        endDate: endDate,
+        endTime: endTime
+      })
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      setResultado(data.resultado);
+      console.log('Response received:', data);
+    } else {
+      const error = await response.text();
+      console.error('Error response:', error);
+      setResultado('Error al crear líneas.');
+    }
+    setIsGenerating(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Generador de Líneas y Creatividades</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="orden">Order Number:</label>
+        <input 
+          type="text" 
+          id="orden" 
+          value={orden} 
+          onChange={(e) => setOrden(e.target.value)} 
+          required 
+        />
+        <label htmlFor="lineNameTemplate">Line Name:</label>
+        <input 
+          type="text" 
+          id="lineNameTemplate" 
+          value={lineNameTemplate} 
+          onChange={(e) => setLineNameTemplate(e.target.value)} 
+          required 
+        />
+        <label htmlFor="lineItemType">Line Type:</label>
+        <input 
+          type="text" 
+          id="lineItemType" 
+          value={lineItemType} 
+          onChange={(e) => setLineItemType(e.target.value.toUpperCase())} 
+          required 
+        />
+        <label htmlFor="priority">Priority value:</label>
+        <input 
+          type="number" 
+          id="priority" 
+          value={priority} 
+          onChange={(e) => setPriority(e.target.value)} 
+          required 
+        />
+        <label htmlFor="inventoryOption">Inventory Option:</label>
+        <select 
+          id="inventoryOption" 
+          value={inventoryOption} 
+          onChange={(e) => setInventoryOption(e.target.value)} 
+          required
         >
-          Learn React
-        </a>
-      </header>
+          <option value="">Selecciona una opción</option>
+          <option value="include">Include</option>
+          <option value="exclude">Exclude</option>
+        </select>
+        {inventoryOption === 'include' && (
+          <>
+            <label htmlFor="inventoryInclude">Inventory Include:</label>
+            <select 
+              id="inventoryInclude" 
+              value={inventoryInclude} 
+              onChange={(e) => setInventoryInclude(e.target.value)} 
+              required
+            >
+              <option value="">Selecciona una opción</option>
+              <option value="LV">LV</option>
+              <option value="MD">MD</option>
+              <option value="RAC1">RAC1</option>
+            </select>
+          </>
+        )}
+        <label htmlFor="expectedCreative">Expected Creative Size:</label>
+        <select 
+          id="expectedCreative" 
+          value={expectedCreative} 
+          onChange={(e) => setExpectedCreative(e.target.value)} 
+          required
+        >
+          <option value="">Selecciona una opción</option>
+          <option value="728x90">728x90</option>
+          <option value="1x1">1x1</option>
+        </select>
+        <label htmlFor="deliverySettings">Delivery Settings:</label>
+        <select 
+          id="deliverySettings" 
+          value={deliverySettings} 
+          onChange={(e) => setDeliverySettings(e.target.value)} 
+          required
+        >
+          <option value="IMMEDIATELY">IMMEDIATELY</option>
+          <option value="CUSTOM">Custom Date and Time</option>
+        </select>
+        {deliverySettings === 'CUSTOM' && (
+          <>
+            <input 
+              type="text" 
+              id="customDate" 
+              placeholder="DD/MM/YYYY" 
+              value={customDate}
+              onChange={(e) => setCustomDate(e.target.value)} 
+              required 
+            />
+            <input 
+              type="time" 
+              id="customTime" 
+              value={customTime}
+              onChange={(e) => setCustomTime(e.target.value)} 
+              required 
+            />
+          </>
+        )}
+        <label htmlFor="endSettings">End Settings:</label>
+        <select 
+          id="endSettings" 
+          value={endSettings} 
+          onChange={(e) => setEndSettings(e.target.value)} 
+          required
+        >
+          <option value="UNLIMITED">UNLIMITED</option>
+          <option value="CUSTOM">Custom End Date and Time</option>
+        </select>
+        {endSettings === 'CUSTOM' && (
+          <>
+            <input 
+              type="text" 
+              id="endDate" 
+              placeholder="DD/MM/YYYY" 
+              value={endDate}
+              onChange={(e) => setEndDate(e.target.value)} 
+              required 
+            />
+            <input 
+              type="time" 
+              id="endTime" 
+              value={endTime}
+              onChange={(e) => setEndTime(e.target.value)} 
+              required 
+            />
+          </>
+        )}
+        <label htmlFor="price">Price:</label>
+        <input 
+          type="number" 
+          id="price" 
+          value={price} 
+          onChange={(e) => setPrice(parseFloat(e.target.value))} 
+          required 
+        />
+        <label htmlFor="limit">Limit:</label>
+        <input 
+          type="number" 
+          id="limit" 
+          value={limit} 
+          onChange={(e) => setLimit(parseFloat(e.target.value))} 
+          required 
+        />
+        <button type="submit" disabled={isGenerating}>Crear Línea</button>
+      </form>
+      {isGenerating && <p>Creando líneas...</p>}
+      {resultado && <p>{resultado}</p>}
+      <ul>
+        {lineItems.map((lineItem) => (
+          <li key={lineItem.id}>{lineItem.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
